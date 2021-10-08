32cc07433873b825972f3700687107d961bb2885
MeztliRA <MeztliRA@gmail.com>
2021-10-08 10:18:35 +0700

    bump version

--- constants/constants.go
+++ constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	Version           = "v1.7.0"
+	Version           = "v1.8.0"
 	CommandMessage    = "what do you want to do(view, add, delete, clear, version, help, quit) "
 	HelpMessageHeader = "\nCommand:"
 	HelpMessage       = "\n\tview: view your notes\n\tadd: add a note\n\tdelete: delete a note\n\tclear: delete all note\n\tversion: show version info\n\thelp: show help message\n\tquit: quit the app"
